fd3b9dedad1a7fd20922ce04a6178afe0b89bb77
mikolajmusiol <mikolajm575@gmail.com>
2025-05-04 18:46:19 +0200

    Database basic integration

---
+++ DSaA_Project_TimeTracker/DSaA_Project_TimeTracker/Database/TTDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Configuration;
+using DSaA_Project_TimeTracker.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DSaA_Project_TimeTracker.Database;
+
+public class TTDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Role> Roles { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = ConfigurationManager.ConnectionStrings["TTDB"].ConnectionString;
+        optionsBuilder.UseSqlServer(connectionString);
+    }
+}
---
+++ DSaA_Project_TimeTracker/DSaA_Project_TimeTracker/Database/UserRepo.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AutoMapper;
+using AutoMapper.Extensions.ExpressionMapping;
+using DSaA_Project_TimeTracker.Database.Entities;
+using DSaA_Project_TimeTracker.DTOs;
+using Microsoft.EntityFrameworkCore;
+
+namespace DSaA_Project_TimeTracker.Database
+{
+    public class UserRepo
+    {
+        private readonly IMapper _mapper;
+
+        public UserRepo()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<TTMappingProfile>();
+            });
+
+            _mapper = config.CreateMapper();
+        }
+
+        public async Task<IEnumerable<UserDto>> GetAll()
+        {
+            using (var context = new TTDbContext())
+            {
+                var users = await context.Users
+                    .Include(x => x.Role)
+                    .ToListAsync();
+
+                if (users is null)
+                {
+                    return new List<UserDto>();
+                }
+                else
+                {
+                    var userDtos = _mapper.Map<List<UserDto>>(users);
+                    return userDtos;
+                }
+                    
+            }
+        }
+
+        public Task<UserDto> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Add(AddUserDto addUserDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateById(int id, UpdateUserDto updateUserDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
